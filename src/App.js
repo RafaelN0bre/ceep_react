da739eed66ed0e7314c474731c7bafb38495b29f
Rafael Nobre <rafaelmedeirosnobre2001@gmail.com>
2021-11-10 15:09:11 -0300

    Controlando o estado e a renderização

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
+  
+  constructor(){
+    super()
+    this.state = {
+      notas:[]
+    }
+  }
+
+
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto}
+    const novoArray = [...this.state.notas, novaNota]
+    const NovoEstado = {
+      notas: novoArray
+    }    
+    this.setState(NovoEstado)
+  }
+  
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota = {this.criarNota.bind(this)}/>
+        <ListaDeNotas notas = {this.state.notas}/>
       </section>
     );
   }
