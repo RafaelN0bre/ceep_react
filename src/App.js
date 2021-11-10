1c0215b1385149cc9aa83167fe8725977e2675ac
Rafael Nobre <rafaelmedeirosnobre2001@gmail.com>
2021-11-10 10:34:22 -0300

    Componentizando o Sistema

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ListaDeNotas from './Components/ListaDeNotas';
+import FormularioCadastro from './Components/FormularioCadastro'
 
-function App() {
-  return (  
-    
-  );
+class App extends Component{
+  render(){
+    return (  
+      <>
+        <FormularioCadastro/>
+        <ListaDeNotas/>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/ListaDeNotas.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { Component } from "react"
+import CardNota from "./CardNota"
+
+export class ListaDeNotas extends Component{
+    render(){
+        return(
+            <ul>
+                {Array.of("Trabalho", "Trabalho", "Estudos")
+                    .map(categoria =>{
+                        return(
+                            <li>
+                                <div>{categoria}</div>
+                                <CardNota/>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+}
+
+export default ListaDeNotas;
